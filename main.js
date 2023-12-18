29485b6f4efca955759feae44ff73c67981e6cd1
Marco Tuzza <marcotuzza@hotmail.it>
2023-12-18 23:30:47 +0100

    Madethe scene and the object responsive

--- main.js
+++ main.js
@@ -13,18 +13,45 @@ const material = new THREE.MeshStandardMaterial(
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
 //Light
 const light = new THREE.PointLight(0xffffff, 70, 100, 1.7);
-light.position.set(0, 0, 10);
+light.position.set(0, 10, 10);
 scene.add(light);
 
 //Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 1000);
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 20;
 scene.add(camera);
 
 //Renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+//Resize
+window.addEventListener("resize", () => {
+  //Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  //Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  //Render
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+}
+loop();
