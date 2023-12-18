32c96d56bec273d3f45bcd46932eb87e37643cb9
Marco Tuzza <marcotuzza@hotmail.it>
2023-12-19 00:23:33 +0100

    Added some roughness to the sphere and animations when user move to cursor

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial( 
   {
-    color: "red"
+    color: "red",
+    roughness: 0.4,
   } 
 );
 const mesh = new THREE.Mesh(geometry, material);
@@ -24,6 +25,7 @@ const sizes = {
 //Light
 const light = new THREE.PointLight(0xffffff, 70, 100, 1.7);
 light.position.set(0, 10, 10);
+light.intensity = 100;
 scene.add(light);
 
 //Camera
@@ -74,3 +76,22 @@ const tl = gsap.timeline({defaults: {duration: 2}});
 tl.fromTo(mesh.scale,{x: 0, y: 0, z: 0},{x: 1, y: 1, z: 1});
 tl.fromTo('nav', {y: "-100%"}, {y:"0%"})
 tl.fromTo('#title', {opacity: 0}, {opacity: 1})
+
+// Cursor
+let mouseDown = false;
+let rgb = [];
+
+window.addEventListener('mousedown', () => {
+  mouseDown = true;
+})
+window.addEventListener('mouseup', () => {
+  mouseDown = false;
+})
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+    rgb = [Math.round((e.pageX / sizes.width) * 255), Math.round((e.pageY / sizes.height) * 255), Math.round(((e.pageX + e.pageY) / (sizes.width + sizes.height)) * 255)];
+    let newColor = new THREE.Color(rgb[0] / 255, rgb[1] / 255, rgb[2] / 255);
+    gsap.to(mesh.material.color, newColor);
+  }
+}
+)
